56acbf2f74e503965e1a871f2135e5dd7fb08b0f
Eva Sharma <20bcs043@nith.ac.in>
2021-09-10 10:39:51 +0530

    User authentication designed with db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const User = require("./models/User");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.json());
+
 app.get("/blog", (req,res)=>{
     res.send("Helloo");
 });
@@ -15,8 +18,37 @@ app.post("/blog", (req,res)=>{
 });
 
 app.post("/login", (req,res)=>{
-    console.log(req.body);
+    
     res.send(req.body);
+    const input = JSON.parse(JSON.stringify(req.body));
+    // console.log(input);
+    if(input.name){
+        const user = new User({
+            name: input.name,
+            email: input.email,
+            password: input.password,
+        } );
+        user.save(function(err){
+            if(err){
+                console.log(err);
+            } else{
+                console.log(`${input.name}'s data saved.`)
+            }
+        });
+    } else{
+        User.find({email: input.email, password: input.password}, function(err,user){
+            if(err){
+                console.log(err);
+            } else{
+                if(!user[0]){
+                    console.log("user not found");
+                } else{
+                    console.log(user);
+                }
+                
+            }
+        });
+    }
 })
 app.listen(8000, ()=>{
     console.log("Server started at port 8000");
--- server/models/User.js
+++ server/models/User.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
+console.log("Mongoose connection open");
 
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    blogs: {
-        imgURL: String,
-        title: String,
-        content: String,
-    },
+    blogs: [mongoose.Schema.Types.Mixed],
 });
 
 module.exports = mongoose.model('User', userSchema);
