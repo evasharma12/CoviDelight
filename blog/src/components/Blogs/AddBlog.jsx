d64a703cdede86e8d1a532cf9ec12b8badb576ab
Eva Sharma <20bcs043@nith.ac.in>
2021-09-16 19:03:01 +0530

    Designed add blog page

--- blog/src/components/Blogs/AddBlog.jsx
+++ blog/src/components/Blogs/AddBlog.jsx
@@ -6,7 +6,7 @@ function AddBlog(user) {
     title: "",
     category: "",
     imageURL:
-      "https://i.pinimg.com/564x/e9/dd/9e/e9dd9e2ac07e10dfce2165fd161bcdec.jpg",
+      "https://i.pinimg.com/564x/48/af/a9/48afa93ab92e258869881e24c6da314e.jpg",
     content: "",
   });
 
@@ -27,57 +27,83 @@ function AddBlog(user) {
   return (
     <div id="add-blog">
       <div id="create-blog">
-        <h1 id = "add-blog-head"><i class="fas fa-pen-fancy"></i>    Unleash your writer</h1>
+        <h1 id="add-blog-head">
+          <i class="fas fa-pen-fancy"></i> Unleash your writer
+        </h1>
         <form id="blog-input" method="post" action="/add-blog">
-          <input id = "addBlog-input" type="text" value={user.id} name="id"  style = {style} required/>
-          <input id = "addBlog-input" type="text" value={user.name} name="name" style = {style} required/>
           <input
-          id = "addBlog-input"
-            onChange={handleInput}
+            id="addBlog-input"
             type="text"
-            placeholder="Title of your Blog"
-            name="title"
+            value={user.id}
+            name="id"
+            style={style}
             required
           />
-          <select
-          id = "addBlog-input"
-            onChange={handleInput}
-            type="text"
-            placeholder="Choose Category"
-            name="category"
-          >
-            <option>Food</option>
-            <option>Travel</option>
-            <option>Health and fitness</option>
-            <option>Lifestyle</option>
-            <option>Technology</option>
-            <option>Business</option>
-            <option>Parenting</option>
-            <option>Hobbies</option>
-            <option>Current affairs</option>
-          </select>
           <input
-          id = "addBlog-input"
-            onChange={handleInput}
+            id="addBlog-input"
             type="text"
-            placeholder="Enter image URL"
-            name="imageURL"
+            value={user.name}
+            name="name"
+            style={style}
+            required
           />
-          <div id = "img-div">
-          <img id = "addBlog-img" src={data.imageURL} alt="blog-img" />
+          <div id="flex3">
+            <div className="flex3-left">
+              <input
+                id="addBlog-input"
+                onChange={handleInput}
+                type="text"
+                placeholder="Title of your Blog"
+                name="title"
+                required
+              />
+              <select
+                id="addBlog-input"
+                onChange={handleInput}
+                type="text"
+                placeholder="Choose Category"
+                name="category"
+              >
+                <option>Lifestyle</option>
+                <option>Food</option>
+                <option>Travel</option>
+                <option>Health and fitness</option>
+
+                <option>Technology</option>
+                <option>Business</option>
+                <option>Parenting</option>
+                <option>Hobbies</option>
+                <option>Current affairs</option>
+              </select>
+              <input
+                id="addBlog-input"
+                onChange={handleInput}
+                type="text"
+                placeholder="Enter image URL"
+                name="imageURL"
+              />
             </div>
+            <div className="flex3-right">
+            <div id="img-div">
+            <img id="addBlog-img" src={data.imageURL} alt="blog-img" />
+          </div>
+            </div>
+          </div>
+
           
+
           <textarea
-          id = "addBlog-input"
-          class = "add-content"
+            id="addBlog-input"
+            class="add-content"
             onChange={handleInput}
             type="text"
             placeholder="Write your content here"
             name="content"
             required
           />
-          <div id = "submit-blog-div"><button id = "submit-blog">Submit</button></div>
-          
+          <div id="submit-blog-div">
+            <button id="submit-blog">Publish</button>
+          </div>
         </form>
       </div>
     </div>
