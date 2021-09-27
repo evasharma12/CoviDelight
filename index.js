cfc8a63048c3b6c77baad9c3004bca6f9782969b
Eva Sharma <20bcs043@nith.ac.in>
2021-09-27 18:34:42 +0530

    deploying

--- index.js
+++ index.js
@@ -136,8 +136,10 @@ app.get("/tour",(req,res)=>{
 
 app.get('*',(req,res)=>{
   res.send("Error!");
-})
-app.listen(process.env.PORT || 8000, () => {
+});
+
+const port  = process.env.PORT || 8000;
+app.listen(port, () => {
   console.log("Server started at port 8000");
 });
 
